8413b3ac55d3f06102f6287d654821b008f7b148
taekow <taeko.owaki@outlook.com>
2022-07-29 16:55:06 -0700

    Fixed cancel button in edit page

--- src/main/java/com/pokemonteambuilder/controllers/PokemonTeamController.java
+++ src/main/java/com/pokemonteambuilder/controllers/PokemonTeamController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.pokemonteambuilder.models.Box;
